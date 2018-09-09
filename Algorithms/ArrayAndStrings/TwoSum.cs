c91aa3b5358f8fcb9d41246fe6e5a43c0e1f8cf6
Dmytro Kolomiets <>
2018-09-09 15:32:56 +0300

    Two sum

---
+++ Algorithms/ArrayAndStrings/TwoSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.ArrayAndStrings
+{
+    public class TwoSum
+    {
+        public int[] Solve(int[] nums, int target) {
+            var dictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+                if (dictionary.ContainsKey(complement))
+                    return new []{dictionary[complement], i};
+                
+                dictionary.TryAdd(nums[i], i);
+            }
+                    
+            throw new Exception();
+        }
+    }
+}
