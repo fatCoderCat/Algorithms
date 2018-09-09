7ba46ea5891a04ad42cf2cbd92c204e2e740e8fe
Dmytro Kolomiets <>
2018-09-09 17:07:37 +0300

    Reverse words in a string

--- Algorithms/ArrayAndStrings/ReverseWordsInString.cs
+++ Algorithms/ArrayAndStrings/ReverseWordsInString.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace Algorithms.ArrayAndStrings
 {
@@ -6,6 +7,26 @@ namespace Algorithms.ArrayAndStrings
     {
         public string Solve1(string s) {
             return string.Join(" ", s.Trim().Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Reverse());
+        }
+        
+        public string Solve2(string s)
+        {
+            var words = s.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+            
+            var i = 0;
+            var j = words.Length - 1;
+            var res = new string[words.Length];
+            
+            while (i <= j)
+            {
+                res[i] = words[j];
+                res[j] = words[i];
+
+                i++;
+                j--;
+            }
+
+            return string.Join(" ", res);
         }
     }
 }
