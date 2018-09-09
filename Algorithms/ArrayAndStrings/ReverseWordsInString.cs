f497e1473978887daf3f022eab8c8b373c5ce0da
Dmytro Kolomiets <>
2018-09-09 15:41:15 +0300

    Reverse words in a string. Linq solution

---
+++ Algorithms/ArrayAndStrings/ReverseWordsInString.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace Algorithms.ArrayAndStrings
+{
+    public class ReverseWordsInString
+    {
+        public string Solve1(string s) {
+            return string.Join(" ", s.Trim().Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Reverse());
+        }
+    }
+}
