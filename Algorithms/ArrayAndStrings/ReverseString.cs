9b65f2f8eec60b33c19d8412316d0fcda15d56da
Dmytro Kolomiets <>
2018-09-09 15:38:52 +0300

    Reverse string

---
+++ Algorithms/ArrayAndStrings/ReverseString.cs
@@ -0,0 +1,25 @@
+﻿namespace Algorithms.ArrayAndStrings
+{
+    public class ReverseString
+    {
+        public string Solve(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return s;
+            var i = 0;
+            var j = s.Length - 1;
+            var res = new char[s.Length];
+            
+            while (i <= j)
+            {
+                res[i] = s[j];
+                res[j] = s[i];
+
+                i++;
+                j--;
+            }
+
+            return new string(res);
+        }
+    }
+}
