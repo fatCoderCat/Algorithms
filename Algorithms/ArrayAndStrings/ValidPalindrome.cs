e066169eae0d336597a874be9e25bcf6d02b68ab
Dmytro Kolomiets <>
2018-09-10 22:44:33 +0300

    Valid palindrome. Reverse half

---
+++ Algorithms/ArrayAndStrings/ValidPalindrome.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace Algorithms.ArrayAndStrings
+{
+    public class ValidPalindrome
+    {
+        public bool Solution(string s)
+        {
+            if(s.Length < 2)
+                return true;
+            
+            var tmp = string.Join("", s.ToLower().Where(x => char.IsLetter(x) || char.IsDigit(x)));
+            int midpoint = tmp.Length / 2;
+             
+            return string.Join("", tmp.Substring(midpoint + tmp.Length % 2).Reverse()) == tmp.Substring(0, midpoint);
+        }
+    }
+}
