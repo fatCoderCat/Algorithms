f18406b1796c82fbda87e818a3c089300e1d4562
Dmytro Kolomiets <>
2018-09-10 23:00:19 +0300

    Valid palindrome: two pointer

--- Algorithms/ArrayAndStrings/ValidPalindrome.cs
+++ Algorithms/ArrayAndStrings/ValidPalindrome.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace Algorithms.ArrayAndStrings
 {
@@ -13,6 +14,46 @@ namespace Algorithms.ArrayAndStrings
             int midpoint = tmp.Length / 2;
              
             return string.Join("", tmp.Substring(midpoint + tmp.Length % 2).Reverse()) == tmp.Substring(0, midpoint);
+        }
+
+        public bool Solution1(string s)
+        {
+            if(s.Length < 2)
+                return true;
+            
+            var i = 0;
+            var j = s.Length - 1;
+            
+            while (i <= j)
+            {
+                var passI = !char.IsLetter(s[i]) && !char.IsDigit(s[i]);
+                var passJ = !char.IsLetter(s[j]) && !char.IsDigit(s[j]);
+                
+                if (passJ && passI)
+                {
+                    i++;
+                    j--;
+                    continue;
+                }
+                if (passI)
+                {
+                    i++;
+                    continue;
+                }
+                if (passJ)
+                {
+                    j--;
+                    continue;
+                }
+
+                if(char.ToLower(s[i]) != char.ToLower(s[j]))
+                    return false;
+                
+                i++;
+                j--;
+            }
+
+            return true;
         }
     }
 }
